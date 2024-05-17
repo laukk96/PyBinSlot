ec02be8236414e98885f41e9702ef325cfb1b402
Kiaran Lau <76144488+pluffpenguin@users.noreply.github.com>
2024-05-17 14:34:28 -0700

    adding outputpath variable

--- binreader.py
+++ binreader.py
@@ -1,11 +1,12 @@
-filepath = './data0002.bin'
+filepath = './data0002.binslot'
+outputpath = './output_binslot.txt'
 data = 0
 
 with open(filepath, 'rb') as file: 
     data = file.readlines()
-    
-    
-with open("output.txt", 'w') as output:
+
+print(f'{outputpath} Creating data of length: {len(data)}')
+with open(outputpath, 'w') as output:
     for i in range(len(data)):
         output.write( str(data[i]) )
         output.write('\n')
